9a8c81cbe523ce2b38f0b4c5f6364eaa558df008
Christopher Dewan <40928458+Dewanism@users.noreply.github.com>
2024-07-15 16:32:19 -0700

    Update 1. Printing.lua
    added different methods of Hello World!

--- Beginner to Pro Series 2018/1. Printing.lua
+++ Beginner to Pro Series 2018/1. Printing.lua
@@ -1,1 +1,12 @@
+-- This program just outputs hello world in different ways.
+
+-- Basic Hello World
 print("Hello World!")
+
+-- Does the same thing as above 
+print('Hello World!') 
+
+-- Also does the same thing as above
+print([[Hello World]])
+
+
