7db65ad344d1a107dd79601f86128d5fcd8cddf6
Christopher Dewan <40928458+Dewanism@users.noreply.github.com>
2024-07-15 16:53:58 -0700

    Update 1. Printing.lua

--- Beginner to Pro Series 2018/1. Printing.lua
+++ Beginner to Pro Series 2018/1. Printing.lua
@@ -9,4 +9,8 @@ print('Hello World!')
 -- Also does the same thing as above
 print([[Hello World]])
 
+-- Adds the values of 9 and 11, then prints output of 20.
+print(9+11)
+
+print("Hello World! ".. "Just kidding.")
 
